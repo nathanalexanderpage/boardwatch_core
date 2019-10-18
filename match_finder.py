85dd7661634bc3fb42d04b552c21197f88016167
nathanalexanderpage <nathanalexanderpage@gmail.com>
2019-10-18 02:12:15 -0700

    basic match score calculation functional

--- match_finder.py
+++ match_finder.py
@@ -1,39 +1,62 @@
 class MatchFinder():
-    def __init__(self):
-        self.type = 'all-purpose'
+	def __init__(self, want):
+		self.type = 'all-purpose'
+		self.want = want
 
-    def assess_match(self, result):
-        match_score = 0.9
-        for match_keyword_strong in result['match_strong']:
-            if match_keyword_strong in result['title']:
-                match_score *= 1.5
-        for match_keyword_weak in result['match_weak']:
-            if match_keyword_weak in result['title']:
-                match_score *= 1.2
-        return match_score
+	def assess_match(self, result):
+		match_score = 0.9
+		for match_keyword_strong in self.want['match_strong']:
+			if match_keyword_strong.lower() in result['title']:
+				match_score *= 1.5
+				break
+		for match_keyword_weak in self.want['match_weak']:
+			if match_keyword_weak.lower() in result['title']:
+				match_score *= 1.2
+				break
+		return match_score
+
+class ConsoleMatchFinder(MatchFinder):
+	def __init__(self):
+		self.type = 'console'
+
+	def assess_match(self, result):
+		print('this is where a match score will be calculated for consoles once the child class is filled out. for now, use parent class MatchFinder.')
 
 if __name__ == '__main__':
-    print('running ' + __file__)
+	print('running ' + __file__)
 
-    example_wants = []
-    example_want1 = {
-        'item': 'PlayStation 4',
-        'type': 'console',
-        'company': 'Sony',
-        'match_strong': [
-            'PS4',
-            'PlayStation 4',
-            'PlayStation4',
-            'Play Station 4',
-            'PS 4'
-        ],
-        'match_weak': [
-            'PlayStation',
-            'Play Station',
-            'PS'
-        ]
-    }
-    example_wants.append(example_want1)
+	example_wants = []
+	example_want1 = {
+		'item': 'PlayStation 4',
+		'type': 'console',
+		'company': 'Sony',
+		'match_strong': [
+			'PS4',
+			'PlayStation 4',
+			'PlayStation4',
+			'Play Station 4',
+			'PS 4'
+		],
+		'match_weak': [
+			'PlayStation',
+			'Play Station',
+			'PS'
+		]
+	}
+	example_wants.append(example_want1)
 
-    print(example_want['item'])
-    # matches = [x for x in lst if fulfills_some_condition(x)]
+	import pprint
+	pp = pprint.PrettyPrinter(indent=2)
+	
+	result = {
+		'datetime': '2019-10-16 20:08',
+		'neighborhood': '(Bryant)',
+		'price': 0,
+		'title': 'playstation 4 pro',
+		'url': 'https://seattle.craigslist.org/see/vgm/d/seattle-playstation-4-pro/6999023452.html'
+	}
+
+	pp.pprint(example_want1)
+	matcher = MatchFinder(example_want1)
+	print(matcher.assess_match(result))
+	# matches = [x for x in lst if fulfills_some_condition(x)]
--- result_parser.py
+++ result_parser.py
@@ -4,85 +4,85 @@ from bs4 import BeautifulSoup
 pp = pprint.PrettyPrinter(indent=2)
 
 class ScraperResult():
-    def __init__(self, soup):
-        self.soup = soup
-        self.data = {}
-        self.scrape_data()
+	def __init__(self, soup):
+		self.soup = soup
+		self.data = {}
+		self.scrape_data()
 
-    def __str__(self):
-        return self.data
+	def __str__(self):
+		return self.data
 
-    def scrape_data(self):
-        print('This is when a child class (specific to a certain site) would scrape data from each posting using BeautifulSoup.')
+	def scrape_data(self):
+		print('This is when a child class (specific to a certain site) would scrape data from each posting using BeautifulSoup.')
 
 class CraigslistScraperResult(ScraperResult):
-    def scrape_data(self):
-        title_tag = self.soup.find(class_='result-title')
-        self.data['title'] = title_tag.text.lower().strip()
-        self.data['url'] = title_tag['href'].strip()
-        self.data['datetime'] = self.soup.p.time['datetime'].strip()
-        if self.soup.find(class_='result-price'):
-            self.data['price'] = int(self.soup.find(class_='result-price').text.strip()[1:])
-        else:
-            self.data['price'] = None
-        if self.soup.find(class_='result-hood'):
-            self.data['neighborhood'] = self.soup.find(class_='result-hood').text.strip()
-        else:
-            self.data['neighborhood'] = None
+	def scrape_data(self):
+		title_tag = self.soup.find(class_='result-title')
+		self.data['title'] = title_tag.text.lower().strip()
+		self.data['url'] = title_tag['href'].strip()
+		self.data['datetime'] = self.soup.p.time['datetime'].strip()
+		if self.soup.find(class_='result-price'):
+			self.data['price'] = int(self.soup.find(class_='result-price').text.strip()[1:])
+		else:
+			self.data['price'] = None
+		if self.soup.find(class_='result-hood'):
+			self.data['neighborhood'] = self.soup.find(class_='result-hood').text.strip()
+		else:
+			self.data['neighborhood'] = None
 
 if __name__ == '__main__':
-    print('running ' + __file__)
+	print('running ' + __file__)
 
-    res_html = """
-    <li class="result-row" data-pid="6992715594" data-repost-of="6950119461">
-        <a class="result-image gallery" data-ids="1:00J0J_84quTTLAkBV,1:00a0a_eEsM9B5sLPu,1:00m0m_6bXfaKWFLhW,1:00L0L_cDPr6i9y1YO,1:00D0D_FJKLc3jGxR,1:00z0z_eYwvVsdLg0i,1:00k0k_hBYChJrKyBI,1:00W0W_9ke9RDKvCSc,1:00707_koGBZcPOTFO,1:01717_4YSrlyBc6Ir,1:00S0S_bb3JvNplCNU,1:00N0N_kWEOOzI5JhL,1:00303_4FWvCDAjT9s,1:00m0m_8Onmj9Q45C9,1:00505_5smR9RotjqC,1:00A0A_B1aiM9rfyf,1:00l0l_8GMQe38aCC0,1:00K0K_dM64X8rLQVZ,1:00f0f_7LZLsVkxCcU,1:00t0t_az2WSJscJ04,1:00707_gV9eDUVGDrF,1:00b0b_2CsI03kJPD5,1:00j0j_68AEoMBARzm,1:01414_cqsjoqFQvZg" href="https://seattle.craigslist.org/see/vgm/d/seahurst-xbox-original-console-all/6992715594.html">
-            <span class="result-price">
-                $50
-            </span>
-        </a>
-        <p class="result-info">
-            <span class="icon icon-star" role="button">
-                <span class="screen-reader-text">
-                    favorite this post
-                </span>
-            </span>
-            <time class="result-date" datetime="2019-10-16 17:42" title="Wed 16 Oct 05:42:37 PM">
-                Oct 16
-            </time>
-            <a class="result-title hdrlnk" data-id="6992715594" href="https://seattle.craigslist.org/see/vgm/d/seahurst-xbox-original-console-all/6992715594.html">
-                XBOX Original Console, All Wires, Dance Mat and 2 Controllers
-            </a>
-            <span class="result-meta">
-                <span class="result-price">
-                    $50
-                </span>
-                <span class="result-hood">
-                    (Burien)
-                </span>
-                <span class="result-tags">
-                    <span class="pictag">
-                        pic
-                    </span>
-                </span>
-                <span class="banish icon icon-trash" role="button">
-                    <span class="screen-reader-text">
-                        hide this posting
-                    </span>
-                </span>
-                <span aria-hidden="true" class="unbanish icon icon-trash red" role="button">
-                </span>
-                <a class="restore-link" href="#">
-                    <span class="restore-narrow-text">
-                        restore
-                    </span>
-                    <span class="restore-wide-text">
-                        restore this posting
-                    </span>
-                </a>
-            </span>
-        </p>
-    </li>
-    """
-    res_soup = BeautifulSoup(res_html, 'html.parser')
-    result = ScraperResult(res_soup)
-    result2 = CraigslistScraperResult(res_soup)
+	res_html = """
+	<li class="result-row" data-pid="6992715594" data-repost-of="6950119461">
+		<a class="result-image gallery" data-ids="1:00J0J_84quTTLAkBV,1:00a0a_eEsM9B5sLPu,1:00m0m_6bXfaKWFLhW,1:00L0L_cDPr6i9y1YO,1:00D0D_FJKLc3jGxR,1:00z0z_eYwvVsdLg0i,1:00k0k_hBYChJrKyBI,1:00W0W_9ke9RDKvCSc,1:00707_koGBZcPOTFO,1:01717_4YSrlyBc6Ir,1:00S0S_bb3JvNplCNU,1:00N0N_kWEOOzI5JhL,1:00303_4FWvCDAjT9s,1:00m0m_8Onmj9Q45C9,1:00505_5smR9RotjqC,1:00A0A_B1aiM9rfyf,1:00l0l_8GMQe38aCC0,1:00K0K_dM64X8rLQVZ,1:00f0f_7LZLsVkxCcU,1:00t0t_az2WSJscJ04,1:00707_gV9eDUVGDrF,1:00b0b_2CsI03kJPD5,1:00j0j_68AEoMBARzm,1:01414_cqsjoqFQvZg" href="https://seattle.craigslist.org/see/vgm/d/seahurst-xbox-original-console-all/6992715594.html">
+			<span class="result-price">
+				$50
+			</span>
+		</a>
+		<p class="result-info">
+			<span class="icon icon-star" role="button">
+				<span class="screen-reader-text">
+					favorite this post
+				</span>
+			</span>
+			<time class="result-date" datetime="2019-10-16 17:42" title="Wed 16 Oct 05:42:37 PM">
+				Oct 16
+			</time>
+			<a class="result-title hdrlnk" data-id="6992715594" href="https://seattle.craigslist.org/see/vgm/d/seahurst-xbox-original-console-all/6992715594.html">
+				XBOX Original Console, All Wires, Dance Mat and 2 Controllers
+			</a>
+			<span class="result-meta">
+				<span class="result-price">
+					$50
+				</span>
+				<span class="result-hood">
+					(Burien)
+				</span>
+				<span class="result-tags">
+					<span class="pictag">
+						pic
+					</span>
+				</span>
+				<span class="banish icon icon-trash" role="button">
+					<span class="screen-reader-text">
+						hide this posting
+					</span>
+				</span>
+				<span aria-hidden="true" class="unbanish icon icon-trash red" role="button">
+				</span>
+				<a class="restore-link" href="#">
+					<span class="restore-narrow-text">
+						restore
+					</span>
+					<span class="restore-wide-text">
+						restore this posting
+					</span>
+				</a>
+			</span>
+		</p>
+	</li>
+	"""
+	res_soup = BeautifulSoup(res_html, 'html.parser')
+	result = ScraperResult(res_soup)
+	result2 = CraigslistScraperResult(res_soup)
