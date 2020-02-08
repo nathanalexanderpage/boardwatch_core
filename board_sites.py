e0c63164558d242ef0e6cb84f42c3c49cc8663f4
nathanalexanderpage <nathanalexanderpage@gmail.com>
2020-02-08 13:37:17 -0800

    added board site configs

--- board_sites.py
+++ board_sites.py
@@ -3,5 +3,20 @@ board_sites = [
 		'name': 'Craigslist',
 		'url': 'https://seattle.craigslist.org/',
         'is_supported': True
+	},
+	{
+		'name': 'eBay',
+		'url': 'https://ebay.com',
+		'is_supported': False
+	},
+	{
+		'name': 'OfferUp',
+		'url': 'https://offerup.com',
+		'is_supported': False
+	},
+	{
+		'name': 'Letgo',
+		'url': 'https://letgo.com',
+		'is_supported': False
 	}
 ]
