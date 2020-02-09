2acb75d713c97e36873337e73b79688b6df4f483
nathanalexanderpage <nathanalexanderpage@gmail.com>
2020-02-09 13:25:39 -0800

    renamed path variables to correctly indicate repo root path

--- boardwatch/app.py
+++ boardwatch/app.py
@@ -1,7 +1,7 @@
 import sys
 import pathlib
-path_to_parent = str(pathlib.Path(__file__).resolve().parents[1].absolute())
-sys.path.append(path_to_parent)
+path_to_root = str(pathlib.Path(__file__).resolve().parents[1].absolute())
+sys.path.append(path_to_root)
 
 import boardwatch
 
--- boardwatch/mail/__init__.py
+++ boardwatch/mail/__init__.py
@@ -1,5 +1,5 @@
 import sys
 import pathlib
-path_to_parent = str(pathlib.Path(__file__).resolve().parents[2].absolute())
-sys.path.append(path_to_parent)
+path_to_root = str(pathlib.Path(__file__).resolve().parents[2].absolute())
+sys.path.append(path_to_root)
 import boardwatch
