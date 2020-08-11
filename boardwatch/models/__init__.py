e42524b9ab3d34450a5ad81c7942e2a21bef2f88
Nathan Page <nathanalexanderpage@gmail.com>
2020-08-11 03:00:57 -0700

    removed unnecessary code

--- boardwatch/models/__init__.py
+++ boardwatch/models/__init__.py
@@ -8,11 +8,3 @@ path_to_root = str(pathlib.Path(__file__).resolve().parents[2].absolute())
 sys.path.append(path_to_root)
 
 import boardwatch
-
-load_dotenv(dotenv_path='../../.env')
-POSTGRESQL_USERNAME = os.getenv('POSTGRESQL_USERNAME')
-POSTGRESQL_PASSWORD = os.getenv('POSTGRESQL_PASSWORD')
-POSTGRESQL_PORT = os.getenv('POSTGRESQL_PORT')
-POSTGRESQL_HOST = os.getenv('POSTGRESQL_HOST')
-POSTGRESQL_DBNAME = os.getenv('POSTGRESQL_DBNAME')
-
