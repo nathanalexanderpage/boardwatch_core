e01167dfca0e03106f9b6f51d7e8457fcdabff04
Nathan Page <nathanalexanderpage@gmail.com>
2020-08-13 12:46:37 -0700

    removed concerns not directly related to core functionality in favor of single module package

--- boardwatch/models/game.py
+++
@@ -1,5 +0,0 @@
-class Game():
-    def __init__(self, name, year_first_release, is_bootleg):
-        self.name = name
-        self.year_first_release = year_first_release
-        self.is_bootleg = is_bootleg
