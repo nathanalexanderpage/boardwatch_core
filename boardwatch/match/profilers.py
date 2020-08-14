d99cc7204ddf3288c0ec6d460cfc2d03df9c2ea4
Nathan Page <nathanalexanderpage@gmail.com>
2020-08-13 17:08:56 -0700

    moved all code blindly into single class, to be refactored rather than create subclasses, as all profiles will have exact same functionality once created

--- boardwatch/match/profilers.py
+++ boardwatch/match/profilers.py
@@ -1,8 +1,9 @@
 import pprint
 
 class Profiler():
-	"""parent match finder class -- mostly for rules inheritance; not to be used to find actual matches"""
+	"""profile used for finding a match for a product within a listing"""
 
+	# GENERAL
 	def __init__(self, want, config={}):
 		self.want = want
 		self.config = config
@@ -19,92 +20,20 @@ class Profiler():
 		self.type = 'generic'
 		self.attr = 'this is a catch-all function for the parent class; usually, this is where child classes define instance attributes that are specific to each child class'
 
-		self.build_string_matches()
+		# negative matches
+		self.antimatch_factors = {
+			'anywhere': [],
+			'before': {
+				'space_separated_yes': [],
+				'space_separated_no': []
+			},
+			'after': {
+				'space_separated_yes': [],
+				'space_separated_no': []
+			}
+		}
 
-	def __str__(self):
-		return 'Profiler parent class instance. To be used for testing purposes only.'
-
-	def build_string_matches(self):
-		print('use child class\'s')
-
-	def check_if_weed_out(self, title_compare):
-		for check in self.antimatch_factors['anywhere']:
-			if check in title_compare: return True
-		return False
-
-	def check_if_negative_matches(self, title_compare, positive_match_string):
-		print('CHECKING if \'' + positive_match_string + '\' truly matches with \'' + title_compare + '\'')
-		if self.check_if_weed_out(title_compare): return True
-		if title_compare.find(positive_match_string) > 0:
-			for check in self.antimatch_factors['before']['space_separated_yes']:
-				print('CHECKING IF \'' + check.lower() + ' ' + positive_match_string + '\' in \'' + title_compare + '\'')
-				if check.lower() + ' ' + positive_match_string in title_compare: return True
-			for check in self.antimatch_factors['before']['space_separated_no']:
-				print('CHECKING IF \'' + check.lower() + positive_match_string + '\' in \'' + title_compare + '\'')
-				if check.lower() + positive_match_string in title_compare: return True
-		if title_compare.find(positive_match_string) + len(positive_match_string) < len(title_compare) - 1:
-			for check in self.antimatch_factors['after']['space_separated_yes']:
-				print('CHECKING IF \'' + positive_match_string + ' ' + check.lower() + '\' in \'' + title_compare + '\'')
-				if positive_match_string + ' ' + check.lower() in title_compare: return True
-			for check in self.antimatch_factors['after']['space_separated_no']:
-				print('CHECKING IF \'' + positive_match_string + check.lower() + '\' in \'' + title_compare + '\'')
-				if positive_match_string + check.lower() in title_compare: return True
-		print(positive_match_string)
-		return False
-
-	def match_score_calculate(self, result):
-		stop_matching_positive = False
-		match_score = 1
-		title_compare = result['title'].lower()
-		# positive match multipliers
-		for match_string_exact in self.match_factors['positive']['exact']:
-			if match_string_exact.lower() == title_compare:
-				stop_matching_positive = True
-				match_score *= self.MATCH_MULTIPLIER_EXACT
-				break
-		for match_string_strong in self.match_factors['positive']['strong']:
-			if stop_matching_positive:
-				break
-			if match_string_strong.lower() in title_compare:
-				if not self.check_if_negative_matches(title_compare, match_string_strong.lower()):
-					match_score *= self.MATCH_MULTIPLIER_STRONG
-		for match_string_weak in self.match_factors['positive']['weak']:
-			if stop_matching_positive:
-				break
-			if match_string_weak.lower() in title_compare:
-				print('FOUND: ' + match_string_weak)
-				if not self.check_if_negative_matches(title_compare, match_string_weak.lower()):
-					match_score *= self.MATCH_MULTIPLIER_WEAK
-		for match_string_weak in self.match_factors['positive']['minor']:
-			if stop_matching_positive:
-				break
-			if match_string_weak.lower() in title_compare:
-				stop_matching_positive = True
-				match_score *= self.MATCH_MULTIPLIER_MINOR
-				break
-
-		print(str(match_score))
-		return match_score
-	
-	def assess_match(self, result):
-		if self.match_score_calculate(result) >= self.MATCH_SCORE_THRESHOLD:
-			return True
-		else:
-			return False
-
-	def assess_meta_price_point(self, result):
-		"""determine if match is at or below user's specified price point"""
-		print('assessing price point...')
-		if self.config['price_point'] == None:
-			return True
-		elif result['price'] > self.config['price_point']:
-			return False
-		else:
-			return True
-
-class PlatformProfiler(Profiler):
-	"""for finding consoles"""
-
+	# Platform
 	def __init__(self, want, name_group_members=[], config={}):
 		super().__init__(want)
 		self.type = 'platform'
@@ -177,9 +106,21 @@ class PlatformProfiler(Profiler):
 		}
 		self.name_group_members = name_group_members
 
+		self.build_string_matches()
+
+	# GENERAL
+	def __str__(self):
+		return 'Profiler parent class instance. To be used for testing purposes only.'
+
+	# PLATFORM
 	def __str__(self):
 		return 'Profiler instance for finding ' + self.want['name']
-	
+			
+	# GENERAL
+	def build_string_matches(self):
+		print('use child class\'s')
+
+	# PLATFORM
 	def build_string_matches(self):
 		print('building item match profile...')
 
@@ -255,7 +196,7 @@ class PlatformProfiler(Profiler):
 			self.want['match_strings']['minor'].append(self.want['family_name'])
 
 		# negative matches
-		antimatch_factors = {
+		self.antimatch_factors = {
 			'anywhere': [],
 			'before': {
 				'space_separated_yes': [],
@@ -268,15 +209,80 @@ class PlatformProfiler(Profiler):
 		}
 
 		pprint.pprint(self.want)
-		
+
+	def check_if_weed_out(self, title_compare):
+		for check in self.antimatch_factors['anywhere']:
+			if check in title_compare: return True
+		return False
+
+	def check_if_negative_matches(self, title_compare, positive_match_string):
+		print('CHECKING if \'' + positive_match_string + '\' truly matches with \'' + title_compare + '\'')
+		if self.check_if_weed_out(title_compare): return True
+		if title_compare.find(positive_match_string) > 0:
+			for check in self.antimatch_factors['before']['space_separated_yes']:
+				print('CHECKING IF \'' + check.lower() + ' ' + positive_match_string + '\' in \'' + title_compare + '\'')
+				if check.lower() + ' ' + positive_match_string in title_compare: return True
+			for check in self.antimatch_factors['before']['space_separated_no']:
+				print('CHECKING IF \'' + check.lower() + positive_match_string + '\' in \'' + title_compare + '\'')
+				if check.lower() + positive_match_string in title_compare: return True
+		if title_compare.find(positive_match_string) + len(positive_match_string) < len(title_compare) - 1:
+			for check in self.antimatch_factors['after']['space_separated_yes']:
+				print('CHECKING IF \'' + positive_match_string + ' ' + check.lower() + '\' in \'' + title_compare + '\'')
+				if positive_match_string + ' ' + check.lower() in title_compare: return True
+			for check in self.antimatch_factors['after']['space_separated_no']:
+				print('CHECKING IF \'' + positive_match_string + check.lower() + '\' in \'' + title_compare + '\'')
+				if positive_match_string + check.lower() in title_compare: return True
+		print(positive_match_string)
+		return False
+
+	def match_score_calculate(self, result):
+		stop_matching_positive = False
+		match_score = 1
+		title_compare = result['title'].lower()
+		# positive match multipliers
+		for match_string_exact in self.match_factors['positive']['exact']:
+			if match_string_exact.lower() == title_compare:
+				stop_matching_positive = True
+				match_score *= self.MATCH_MULTIPLIER_EXACT
+				break
+		for match_string_strong in self.match_factors['positive']['strong']:
+			if stop_matching_positive:
+				break
+			if match_string_strong.lower() in title_compare:
+				if not self.check_if_negative_matches(title_compare, match_string_strong.lower()):
+					match_score *= self.MATCH_MULTIPLIER_STRONG
+		for match_string_weak in self.match_factors['positive']['weak']:
+			if stop_matching_positive:
+				break
+			if match_string_weak.lower() in title_compare:
+				print('FOUND: ' + match_string_weak)
+				if not self.check_if_negative_matches(title_compare, match_string_weak.lower()):
+					match_score *= self.MATCH_MULTIPLIER_WEAK
+		for match_string_weak in self.match_factors['positive']['minor']:
+			if stop_matching_positive:
+				break
+			if match_string_weak.lower() in title_compare:
+				stop_matching_positive = True
+				match_score *= self.MATCH_MULTIPLIER_MINOR
+				break
+
+		print(str(match_score))
+		return match_score
+	
+	def assess_match(self, result):
+		if self.match_score_calculate(result) >= self.MATCH_SCORE_THRESHOLD:
+			return True
+		else:
+			return False
+
 	def parentheses_judger(self, comp_title):
 		pass
 		if self.want['abbreviation_official']:
-			antimatch_factors.append('(' + self.want['abbreviation_official'] + ')')
-		antimatch_factors.append('(' + self.want['name'] + ')')
-		antimatch_factors.append('(' + self.want['name_group'] + ')')
+			self.antimatch_factors.append('(' + self.want['abbreviation_official'] + ')')
+		self.antimatch_factors.append('(' + self.want['name'] + ')')
+		self.antimatch_factors.append('(' + self.want['name_group'] + ')')
 		for name in self.want['names_other']:
-			antimatch_factors.append('(' + self.want['name_group'] + ')')
+			self.antimatch_factors.append('(' + self.want['name_group'] + ')')
 
 	# FIXME: account for possibility of platforms of different consoles with same names (PS2 slim, PS4 slim)
 	# FIXME: make sure platform names within the same family are distinguished between when generating matches. make sure a potential match with one is not any of the others
@@ -353,28 +359,3 @@ class PlatformProfiler(Profiler):
 
 if __name__ == '__main__':
 	print('running ' + __file__)
-
-	import pprint
-	from console_data_resource import *
-	
-	pp = pprint.PrettyPrinter(indent=2)
-	
-	result = {
-		'datetime': '2019-10-16 20:08',
-		'neighborhood': '(Bryant)',
-		'price': 0,
-		'title': 'playstation 4 pro',
-		'url': 'https://seattle.craigslist.org/see/vgm/d/seattle-playstation-4-pro/6999023452.html'
-	}
-
-	config = {
-		'price_point': 100
-	}
-
-	pp.pprint(consoles)
-	matcher = PlatformProfiler(wii, {})
-	pp.pprint(matcher.match_factors)
-	pp.pprint(matcher.antimatch_factors)
-	match_score = matcher.match_score_calculate(result)
-	print(match_score)
-	# matches = [x for x in lst if fulfills_some_condition(x)]
