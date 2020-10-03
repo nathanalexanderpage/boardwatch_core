41127c10e58a04b9a442fb067bb98a2527e5cf85
Nathan Page <nathanalexanderpage@gmail.com>
2020-10-03 16:05:53 -0700

    scrapped relative import due to errors

--- boardwatch/app.py
+++ boardwatch/app.py
@@ -3,10 +3,8 @@ import pathlib
 path_to_root = str(pathlib.Path(__file__).resolve().parents[1].absolute())
 sys.path.append(path_to_root)
 
-import boardwatch
-
-from boardwatch.common import board_site_enums
-from boardwatch.scrape.populate_listings import ListingPopulator
-from boardwatch.mail.mail_sender import Mailer
+from common import board_site_enums
+from scrape.populate_listings import ListingPopulator
+from mail.mail_sender import Mailer
 
 print('imports work')
