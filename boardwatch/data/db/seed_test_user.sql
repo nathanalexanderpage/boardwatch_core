d799b2b012af8c7b46d9cf63c6be53f21b9071b9
Nathan Page <nathanalexanderpage@gmail.com>
2020-12-18 01:36:48 -0800

    ordered, pared down query with all necessary digest info for notifications

--- boardwatch/data/db/seed_test_user.sql
+++ boardwatch/data/db/seed_test_user.sql
@@ -23,8 +23,25 @@ INSERT INTO watchlist_platform_editions
     );
 
 SELECT
-    u.id as user_id,
-    u.username as username,
-    wpe.platform_edition_id as watched_platform_edition_id
+    wpe.user_id as user_id,
+    wpe.platform_edition_id as watched_platform_edition_id,
+    pf.name AS platform_family,
+    p.name AS platform,
+    pe.name AS edition,
+    pe.official_color AS official_color,
+    x.colors AS colors
     FROM watchlist_platform_editions as wpe
-    JOIN users as u ON u.id = wpe.user_id;
+    JOIN platform_editions AS pe ON pe.id = wpe.platform_edition_id
+    JOIN platforms AS p ON pe.platform_id = p.id
+    JOIN platform_families AS pf ON pf.id = p.platform_family_id
+    LEFT JOIN platform_name_groups AS png ON png.id = p.name_group_id
+    JOIN generations AS gen ON gen.id = pf.generation
+    JOIN
+        (SELECT pe.id AS id, STRING_AGG(c.name,', ') AS colors
+        FROM platform_editions AS pe
+        JOIN colors_platform_editions AS cpe ON cpe.platform_edition_id = pe.id
+        JOIN colors AS c ON c.id = cpe.color_id
+        GROUP BY pe.id
+        ORDER BY pe.id)
+    AS x ON x.id = pe.id
+    ORDER BY user_id, gen.id, png.name, platform_family, platform;
