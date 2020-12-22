3ff17cd97c872baba847e6c882b486693648c4a0
Nathan Alexander Page <nathanalexanderpage@gmail.com>
2020-12-21 23:54:51 -0800

    further delineate structure for first finding matches, then eliminating all but one match per text segment in listing

--- boardwatch/__main__.py
+++ boardwatch/__main__.py
@@ -47,13 +47,13 @@ for board in Board.get_all():
 # pull all database listings newer than 1 day old
 DataPuller.pull_listings()
 
+# find product instances in listings
 for listing in Listing.get_all():
 	for edition in PlatformEdition.get_all():
 		searchtexts = profiler.build_string_matches(edition)
 		pp.pprint(searchtexts)
 
 		for degree in searchtexts.keys():
-			
 			# for each listing, iterate through all searchable text segments
 			for searchtext in searchtexts[degree]:
 				for text in [listing.title, listing.body]:
@@ -66,16 +66,13 @@ for listing in Listing.get_all():
 						# print(e)
 						continue
 
-		continue
-		
-		# if preliminary match, look in product's name group to check if matched item text isn't suited better for a different product with similar name
+for listing in Listing.get_all():
+	continue
+	# for each match for each listing, cross-check to see if any overlap exists
+	# if overlap, ignore all matches but that with the highest match score (record only the most likely match to db)
 
-		# insert db record to indicate match between listing and each found product
-		for match in Match.matches:
-			if match.type == 'platform':
-				cur.execute("""INSERT INTO listings_platform_editions (listing_id, platform_edition_id) VALUES(%s, %s);""", (listing.id, edition.id,))
-			else:
-				pass
+	# insert db record to indicate match between listing and each found product
+	cur.execute("""INSERT INTO listings_platform_editions (listing_id, platform_edition_id) VALUES(%s, %s);""", (listing.id, edition.id,))
 
 # loop through users. send e-mail notifications only for those whose settings indicate that preference.
 cur.execute("""SELECT wpe.user_id as user_id, pf.name AS platform_family, p.name AS platform, wpe.platform_edition_id as watched_platform_edition_id, pe.name AS edition_name, pe.official_color AS official_color, x.colors AS colors FROM watchlist_platform_editions as wpe JOIN platform_editions AS pe ON pe.id = wpe.platform_edition_id JOIN platforms AS p ON pe.platform_id = p.id JOIN platform_families AS pf ON pf.id = p.platform_family_id LEFT JOIN platform_name_groups AS png ON png.id = p.name_group_id JOIN generations AS gen ON gen.id = pf.generation JOIN (SELECT pe.id AS id, STRING_AGG(c.name,', ') AS colors FROM platform_editions AS pe JOIN colors_platform_editions AS cpe ON cpe.platform_edition_id = pe.id JOIN colors AS c ON c.id = cpe.color_id GROUP BY pe.id ORDER BY pe.id) AS x ON x.id = pe.id ORDER BY user_id, gen.id, png.name, platform_family, platform;""")
